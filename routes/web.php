823d328914f110fd16eb8e5ebddb660391af4db6
Chong <monkey.d.chong@gmail.com>
2019-06-05 10:58:26 +0700

    set: login

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/uploadfile', 'UploadfileController@index');
+Route::post('/uploadfile', 'UploadfileController@upload');
+Route::get('/main', 'MainController@index');
+Route::post('/main/checklogin', 'MainController@checklogin');
+Route::get('main/successlogin', 'MainController@successlogin');
+Route::get('main/logout', 'MainController@logout');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
